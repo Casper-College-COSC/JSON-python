50836e45fd6008a8fab18c3197a264dd0d447eb8
Charlotte Gruner <60362002+charlottegruner@users.noreply.github.com>
2025-07-03 09:08:53 -0600

    Update json_example.py
    Simple json example

--- json_example.py
+++ json_example.py
@@ -4,15 +4,4 @@ with open("json1.txt", "r") as file:
     data = json.load(file)
 
 print(data)
-print(data["hobbies"][0])
-
-with open("json2.txt", "r") as file:
-    people = json.load(file)
-    
-print(people["person"]["address"]["city"])
-
-with open("json3.txt", "r") as file:
-    people = json.load(file)
-    
-print(people["people"][1]["name"])
-print(people["people"][1]["hobbies"][0])
+print(data["hobbies"])  # Output: ['reading', 'gaming']
